3fa805f4b204a3ed81abed3d569517fb588330df
Apurbo73 <apurbodebnath895@gmail.com>
2023-07-28 23:15:56 +0600

    updated products

--- Controller/productController.js
+++ Controller/productController.js
@@ -20,7 +20,7 @@ export const addProduct = async (req, res) => {
 
 export const getAllProducts = async (req, res) => {
   try {
-    console.log(req.body);
+    // console.log(req.body);
     const products = await product.find({}).sort({ createdAt: -1 });
     return res.status(200).json(products);
   } catch (error) {
@@ -31,7 +31,7 @@ export const getAllProducts = async (req, res) => {
 export const getSingleProduct = async (req, res) => {
   try {
     console.log(req.body);
-    const singleProduct = await product.findById(req.params.id)
+    const singleProduct = await product.findById(req.params.id);
     return res.status(200).json(singleProduct);
   } catch (error) {
     return res.status(500).json(error.message);
@@ -40,8 +40,29 @@ export const getSingleProduct = async (req, res) => {
 
 export const deleteProduct = async (req, res) => {
   try {
-    const deleteProduct = await product.findByIdAndDelete(req.params.id)
+    const deleteProduct = await product.findByIdAndDelete(req.params.id);
     return res.status(200).json(deleteProduct);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+
+export const updateProduct = async (req, res) => {
+  console.log(req.body.name);
+  try {
+    const update = await product.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        name: req.body.name,
+        imageLink: req.body.imageLink,
+        category: req.body.category,
+        seller: req.body.seller,
+        price: req.body.price
+      }
+    );
+
+    await update.save();
+    return res.status(200).json(update);
   } catch (error) {
     return res.status(500).json(error.message);
   }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,13 +2,15 @@ import express from "express";
 import {
   addProduct,
   getAllProducts,
-  getSingleProduct,deleteProduct
+  getSingleProduct,
+  deleteProduct,
+  updateProduct
 } from "../Controller/productController.js";
 const route = express.Router();
 route.post("/products", addProduct);
 route.get("/products", getAllProducts);
 route.get("/products/:id", getSingleProduct);
 route.delete("/products/:id", deleteProduct);
-
+route.put("/products/:id", updateProduct);
 
 export default route;
